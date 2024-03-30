0b96eeca8b239303e2bbecfbaea84a2e314659ce
nhanng19 <nhanng19@gmail.com>
2024-03-29 20:29:45 -0700

    initialize project, add navigation bar

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+export default function Home() {
+  return (
+    <>
+    hello!
+    </>
+  );
+}
---
+++ app/(marketing)/test/page.tsx
@@ -0,0 +1,7 @@
+const Test = () => {
+    <>
+    Hello
+    </>
+}
+
+export default Test
