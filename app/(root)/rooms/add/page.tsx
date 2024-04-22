bc4dd4c1efec466e3170eb492d168e2edb2103b0
nhanng19 <nhanng19@gmail.com>
2024-04-22 01:30:32 -0700

    app/(root)/rooms/add/page.tsx updated

--- app/(root)/rooms/add/page.tsx
+++ app/(root)/rooms/add/page.tsx
@@ -37,64 +37,75 @@ const Page = () => {
 
   return (
     <>
-      <h1 className="font-bold mb-4 text-3xl">Start a virtual sales room</h1>
-      <div className="grid grid-cols-3 h-full w-full gap-4 ">
-        {products.length > 0 &&
-          products.map((product: any) => {
-            const itemData = product.item_data;
-            const price =
-              itemData.variations[0].item_variation_data.price_money.amount;
-            const currency =
-              itemData.variations[0].item_variation_data.price_money.currency;
-            const date = new Date(product.updated_at).toLocaleString();
-            return (
-              <div className="relative block overflow-hidden rounded-lg border  p-4 sm:p-6 lg:p-8 bg-background">
-                <span className="absolute inset-x-0 bottom-0 h-2 "></span>
+      <h1 className="font-bold mb-4 text-3xl">Sales room</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Start a sales room</CardTitle>
+          <CardDescription>
+            Select one of your products to start a live, virtual sales room.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid grid-cols-3 h-full w-full gap-4 ">
+            {products.length > 0 &&
+              products.map((product: any) => {
+                const itemData = product.item_data;
+                const price =
+                  itemData.variations[0].item_variation_data.price_money.amount;
+                const currency =
+                  itemData.variations[0].item_variation_data.price_money
+                    .currency;
+                const date = new Date(product.updated_at).toLocaleString();
+                return (
+                  <div className="relative block overflow-hidden rounded-lg border  p-4 sm:p-6 lg:p-8 bg-background">
+                    <span className="absolute inset-x-0 bottom-0 h-2 "></span>
 
-                <div className="sm:flex sm:justify-between sm:gap-4">
-                  <div>
-                    <h3 className="text-lg font-bold sm:text-xl line-clamp-1">
-                      {itemData.name}
-                    </h3>
+                    <div className="sm:flex sm:justify-between sm:gap-4">
+                      <div>
+                        <h3 className="text-lg font-bold sm:text-xl line-clamp-1">
+                          {itemData.name}
+                        </h3>
 
-                    <p className="mt-1 text-xs font-medium ">
-                      {formatMoney(price, currency)}
-                    </p>
+                        <p className="mt-1 text-xs font-medium ">
+                          {formatMoney(price, currency)}
+                        </p>
+                      </div>
+
+                      <div className="hidden sm:block sm:shrink-0">
+                        <img
+                          alt=""
+                          src={itemData.ecom_image_uris}
+                          className="size-16 rounded-lg object-cover shadow-sm"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="mt-4">
+                      <p className="text-pretty text-sm line-clamp-3">
+                        {itemData.description}
+                      </p>
+                    </div>
+
+                    <dl className="mt-6 flex gap-4 sm:gap-6 justify-end">
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button>Go Live</Button>
+                        </DialogTrigger>
+                        <DialogContent className="min-w-full !h-screen">
+                          <StreamLoadingScreen
+                            userId={user?.userId as string}
+                            productName={itemData.name}
+                            catalogId={product.id}
+                          />
+                        </DialogContent>
+                      </Dialog>
+                    </dl>
                   </div>
-
-                  <div className="hidden sm:block sm:shrink-0">
-                    <img
-                      alt=""
-                      src={itemData.ecom_image_uris}
-                      className="size-16 rounded-lg object-cover shadow-sm"
-                    />
-                  </div>
-                </div>
-
-                <div className="mt-4">
-                  <p className="text-pretty text-sm line-clamp-3">
-                    {itemData.description}
-                  </p>
-                </div>
-
-                <dl className="mt-6 flex gap-4 sm:gap-6 justify-end">
-                  <Dialog>
-                    <DialogTrigger asChild>
-                      <Button>Go Live</Button>
-                    </DialogTrigger>
-                    <DialogContent className="min-w-full !h-screen">
-                      <StreamLoadingScreen
-                        userId={user?.userId as string}
-                        productName={itemData.name}
-                        catalogId={product.id}
-                      />
-                    </DialogContent>
-                  </Dialog>
-                </dl>
-              </div>
-            );
-          })}
-      </div>
+                );
+              })}
+          </div>
+        </CardContent>
+      </Card>
     </>
   );
 };
