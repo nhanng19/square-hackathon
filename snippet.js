13142b16668846281ee432e774f5090f47d1520b
nhanng19 <nhanng19@gmail.com>
2024-04-25 04:20:10 -0700

    snippet.js updated

--- snippet.js
+++ snippet.js
@@ -1,5 +1,7 @@
 let previousWindow = null;
 
+const userId = "6629c9f7ab7170b2f9fd979f";
+
 const decodeRoomId = (roomId) => {
   let decodedString = "";
   for (let i = 0; i < roomId.length; i += 2) {
@@ -12,6 +14,21 @@ const createLiveBadge = () => {
   const badge = document.createElement("button");
   badge.textContent = "LIVE";
   badge.style.backgroundColor = "red";
+  badge.style.color = "white";
+  badge.style.padding = "4px 8px";
+  badge.style.borderRadius = "5px";
+  badge.style.marginLeft = "5px";
+  badge.style.textDecoration = "none";
+  badge.style.zIndex = "99";
+  badge.style.border = "none";
+  badge.style.cursor = "pointer";
+  return badge;
+};
+
+const createRecordingBadge = () => {
+  const badge = document.createElement("button");
+  badge.textContent = "RECORDING";
+  badge.style.backgroundColor = "blue";
   badge.style.color = "white";
   badge.style.padding = "4px 8px";
   badge.style.borderRadius = "5px";
@@ -44,20 +61,44 @@ const addLiveBadgeToParagraph = (p, productId) => {
   p.insertAdjacentElement("afterend", badge);
 };
 
+const addRecordingBadgeToParagraph = (p, productId) => {
+  const badge = createRecordingBadge();
+  badge.addEventListener("click", (e) => {
+    e.preventDefault();
+    openLiveStream(productId);
+  });
+  p.insertAdjacentElement("afterend", badge);
+};
+
+const fetchRecordings = async () => {
+  const response = await fetch(
+    `http://localhost:3000/api/recordings/list_recordings/${userId}`
+  );
+  const recordings = await response.json();
+  return recordings;
+};
+
 setTimeout(async () => {
   const response = await fetch(
-    "http://localhost:3000/api/rooms/list_rooms/661cd233a9d7ec9afda8a8a0"
+    `http://localhost:3000/api/rooms/list_rooms/${userId}`
   );
   const items = await response.json();
-
+  const recordings = await fetchRecordings();
   document.querySelectorAll("p").forEach((p) => {
     const itemText = p.textContent.trim();
     const matchingItem = items.find((item) =>
       itemText.includes(decodeRoomId(item?.roomId))
     );
+    const matchingRecording = recordings.find((recording) =>
+      itemText.includes(decodeRoomId(recording?.roomId))
+    );
     if (matchingItem) {
       const productId = matchingItem?.roomId;
       addLiveBadgeToParagraph(p, productId);
+    }
+    if (matchingRecording) {
+      const productId = matchingRecording?.roomId;
+      addRecordingBadgeToParagraph(p, productId);
     }
   });
 }, 2000);
