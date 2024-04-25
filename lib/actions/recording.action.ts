611b8d485a556f5ce0610c0f378d69b1e33e6083
nhanng19 <nhanng19@gmail.com>
2024-04-25 04:20:09 -0700

    lib/actions/recording.action.ts updated

--- lib/actions/recording.action.ts
+++ lib/actions/recording.action.ts
@@ -1,19 +1,22 @@
+"use server"
+
 import Recording from "../models/recording.model";
 import { connectToDB } from "../mongoose"
 
 export interface RecordingProps { 
-    userId: string, 
+    userId: string | undefined, 
     roomId: string,
     fileName: string,
     startTime: string,
     endTime: string,
-    url: string
+    url: string,
+    thumbnail?: string | unknown | undefined,
 }
 
-export const createRecording = async ({ userId, roomId, fileName, startTime, endTime, url } : RecordingProps) => { 
+export const createRecording = async({ userId, roomId, fileName, startTime, endTime, url, thumbnail } : RecordingProps) => { 
     connectToDB();
     try {
-        const recording = await Recording.create({ userId, roomId, fileName, startTime, endTime, url })
+        const recording = await Recording.create({ userId, roomId, fileName, startTime, endTime, url, thumbnail })
         return recording;
     } catch (error) { 
         throw new Error(`Failed to create recording: ${error}`)
@@ -27,5 +30,14 @@ export const getRecordings = async (userId: string) => {
         return recordings;
     } catch (error) { 
         throw new Error(`Failed to get recordings: ${error}`)
+    }
+}
+
+export const deleteRecording = async (recordingId: string) => {
+    connectToDB();
+    try {
+      return await Recording.deleteOne({ _id: recordingId });
+    } catch (error) {
+      throw new Error(`Failed to delete recording: ${error}`);
     }
 }
