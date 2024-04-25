3ca89e847c1104d6a51d77366310c2550fa8990c
nhanng19 <nhanng19@gmail.com>
2024-04-25 04:20:08 -0700

    app/api/recordings/list_recordings/[id]/route.ts updated

--- app/api/recordings/list_recordings/[id]/route.ts
+++ app/api/recordings/list_recordings/[id]/route.ts
@@ -1,5 +1,20 @@
-import { NextRequest } from "next/server";
+import { getRecordings } from "@/lib/actions/recording.action";
+import { errorResponse } from "@/lib/utils";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-    
+    const userId = params.id;
+    try { 
+        const recordings = await getRecordings(userId);
+        return NextResponse.json(recordings, {
+          status: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          },
+        });
+    } catch (error) { 
+        return errorResponse(error)
+    }
 }
