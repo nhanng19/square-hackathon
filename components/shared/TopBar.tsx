ff443be74f41da401b5a4d6be67705ed9d1bcba8
Nhan Nguyen <nhanng19@gmail.com>
2025-05-23 10:51:47 -0700

    new hero section

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -16,7 +16,7 @@ const TopBar = () => {
   return (
     <Sheet>
       <nav className="bg-background border-b fixed top-0 z-10 w-full">
-        <div className="container  xl:px-0 flex flex-wrap items-center justify-between mx-auto p-4 top-0">
+        <div className="container max-w-7xl xl:px-0 flex flex-wrap items-center justify-between mx-auto p-4 top-0">
           <Link
             href="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
@@ -36,38 +36,6 @@ const TopBar = () => {
 
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="justify-center items-center font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Benefits
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Demo
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Testimonials
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  FAQ
-                </a>
-              </li>
               <li>
                 <Button variant="default" asChild>
                   <Link href="/login">Log In</Link>
